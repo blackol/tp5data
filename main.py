a5a084a482f037170724796c6c9dfe552cbfe5f0
blackol <anthonycolvil@gmail.com>
2021-11-30 03:23:19 +0100

    csv to tableau

--- main.py
+++ main.py
@@ -1,48 +1,14 @@
-import csv 
+import csv
 
-# variable qui contient le nom du fichier csv
-file_name = "Absenteeism_at_work.csv"
+file = csv.reader(open('Absenteeism_at_work.csv', 'r'))
 
-#fonction qui convertit un csv en dictionnaire de données et qui renvoie le dictionnaire de données sans module numpy
-def csv_to_dict(file_name):
-    with open(file_name, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        data = []
-        for row in csv_reader:
-            data.append(row)
-    return data
+title = next(file)    
+datas = []
 
-#fonction qui convertit un csv en tableau de données et qui renvoie le tableau de données sans module numpy
-def csv_to_array(file_name):
-    with open(file_name, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file)
-        data = []
-        for row in csv_reader:
-            data.append(row)
-    return data
+for row in file:
+    for i in range(len(row)):
+        temp = row[i].split(";")
+        temp = [float(i) for i in temp]
+        datas.append(temp)
 
-#Afficher le contenu d'un tableau de données
-def print_array(data):
-    for row in data:
-        print(row)
-
-
-#fonction qui affiche le contenu d'un dictionnaire de données
-def print_dict(data):
-    for row in data:
-        print(row)
-
-
-
-#split (transforme une chaine de caractère en tableau)
-
-
-#Programme principale ----------------------------------------------------------------------------------------------------
-
-data = csv_to_array(file_name)
-print(data)
-print(1)
-#data2 = get_column(data, 1)
-#print(data2)
-
-#----------------------------------------------------------------------------------------------------------------------
+print(datas[2][3])
